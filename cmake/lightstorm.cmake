0cf16dad2091f1c0d15d85cb2302359333a89aa0
AlexDenisov <alex@lowlevelbits.org>
2024-11-24 20:49:35 +0100

    Add pre-commit

--- cmake/lightstorm.cmake
+++ cmake/lightstorm.cmake
@@ -1,19 +1,19 @@
 function(add_lightstorm_executable ruby)
-  set (in_ruby ${CMAKE_CURRENT_LIST_DIR}/${ruby})
-  set (out_c ${CMAKE_CURRENT_BINARY_DIR}/${ruby}.c)
-  set (target_name ${ruby}.exe)
-  add_custom_command(OUTPUT ${out_c}
+  set(in_ruby ${CMAKE_CURRENT_LIST_DIR}/${ruby})
+  set(out_c ${CMAKE_CURRENT_BINARY_DIR}/${ruby}.c)
+  set(target_name ${ruby}.exe)
+  add_custom_command(
+    OUTPUT ${out_c}
     COMMAND $<TARGET_FILE:lightstorm> ${in_ruby} -o ${out_c}
-    DEPENDS ${in_ruby} lightstorm
-  )
+    DEPENDS ${in_ruby} lightstorm)
   add_executable(${target_name} ${out_c})
   target_compile_options(${target_name} PRIVATE ${LIGHTSTORM_CFLAGS})
   target_link_options(${target_name} PRIVATE ${LIGHTSTORM_CFLAGS})
-  target_link_libraries(${target_name} PRIVATE mruby_static lightstorm_runtime_main)
-  target_include_directories(${target_name} PRIVATE
-    ${CMAKE_SOURCE_DIR}
-    ${CMAKE_SOURCE_DIR}/third_party/mruby/include
-    ${CMAKE_SOURCE_DIR}/third_party/mruby/build/host/include
-  )
+  target_link_libraries(${target_name} PRIVATE mruby_static
+                                               lightstorm_runtime_main)
+  target_include_directories(
+    ${target_name}
+    PRIVATE ${CMAKE_SOURCE_DIR} ${CMAKE_SOURCE_DIR}/third_party/mruby/include
+            ${CMAKE_SOURCE_DIR}/third_party/mruby/build/host/include)
   add_dependencies(${target_name} mruby_static)
 endfunction()
--- cmake/mruby.cmake
+++ cmake/mruby.cmake
@@ -1,18 +1,19 @@
 include(ExternalProject)
-set (MRUBY_DIR "${CMAKE_SOURCE_DIR}/third_party/mruby")
-set (MRUBY_BINARY "${MRUBY_DIR}/bin/mruby")
-set (MRBC_BINARY "${MRUBY_DIR}/bin/mrbc")
-set (MRUBY_STATIC "${MRUBY_DIR}/build/host/lib/libmruby.a")
+set(MRUBY_DIR "${CMAKE_SOURCE_DIR}/third_party/mruby")
+set(MRUBY_BINARY "${MRUBY_DIR}/bin/mruby")
+set(MRBC_BINARY "${MRUBY_DIR}/bin/mrbc")
+set(MRUBY_STATIC "${MRUBY_DIR}/build/host/lib/libmruby.a")
 
-ExternalProject_Add(mruby
+ExternalProject_Add(
+  mruby
   SOURCE_DIR ${MRUBY_DIR}
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${CMAKE_COMMAND} -E env CFLAGS="${LIGHTSTORM_CFLAGS}" LDFLAGS="${LIGHTSTORM_CFLAGS}" rake all --verbose
+  BUILD_COMMAND ${CMAKE_COMMAND} -E env CFLAGS="${LIGHTSTORM_CFLAGS}"
+                LDFLAGS="${LIGHTSTORM_CFLAGS}" rake all --verbose
   BUILD_IN_SOURCE ON
   INSTALL_COMMAND ""
   BUILD_BYPRODUCTS ${MRUBY_STATIC} ${MRUBY_BINARY} ${MRBC_BINARY}
-  BUILD_ALWAYS
-)
+  BUILD_ALWAYS)
 
 add_executable(mruby_binary IMPORTED GLOBAL)
 set_property(TARGET mruby_binary PROPERTY IMPORTED_LOCATION ${MRUBY_BINARY})
